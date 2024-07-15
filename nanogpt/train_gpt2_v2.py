a4d1bb396e3bc016b7a753c9876237fa7b814b33
ayushrajdahal <54719576+ayushrajdahal@users.noreply.github.com>
2024-07-15 13:22:55 -0500

    Change vocab size to 50304 for optimization

--- nanogpt/train_gpt2_v2.py
+++ nanogpt/train_gpt2_v2.py
@@ -14,7 +14,7 @@ def timer_end():
 @dataclass
 class GPTConfig:
     block_size: int = 1024      # max sequence length
-    vocab_size: int = 50257     # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
+    vocab_size: int = 50304     # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token; NOTE: was initially 50257, changed into 50304 for optimization
     n_layer: int = 12           # number of layers
     n_head: int = 12            # number of heads
     n_embd: int = 768           # embedding dimension
