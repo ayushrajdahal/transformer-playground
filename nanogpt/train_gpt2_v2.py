6421fc8fa1a62e80f03a5c6b17ab351111652311
ayushrajdahal <54719576+ayushrajdahal@users.noreply.github.com>
2024-07-15 00:14:16 -0500

    Add DataLoaderLite, sample using unique data in each batch

--- nanogpt/train_gpt2_v2.py
+++ nanogpt/train_gpt2_v2.py
@@ -148,7 +148,31 @@ class GPT(nn.Module):
 
         return model
 
+# --------------------------------------------------------------------------------------------------------------------------------
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        self.tokens = torch.tensor(enc.encode(text), dtype=torch.long)
+        print(f"loaded {len(self.tokens)} tokens\n{len(self.tokens)//(B*T)} batches of size {B}x{T}")
+        
+        # state
+        self.current_position = 0
 
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position:self.current_position+B*T+1] # +1 because of the target token
+        x = buf[:-1].view(B, T) # inputs
+        y = buf[1:].view(B, T) # targets
+        # advance the position in the tensor
+        self.current_position += B*T
+        # if loading the next batch would be out of bounds, reset
+        if self.current_position + B*T + 1 > len(self.tokens):
+            self.current_position = 0
+        return x, y
 # --------------------------------------------------------------------------------------------------------------------------------
 
 device = "cpu"
@@ -158,35 +182,23 @@ elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print("device:", device)
 
-
-# get a data batch
-
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-with open('input.txt', 'r') as f:
-    text = f.read()
-
-text = text[:1000]
-tokens = enc.encode(text)
-B,T = 4, 32
-buf = torch.tensor(tokens[:B*T+1], dtype=torch.long, device=device)
-x = buf[:-1].view(B, T)
-y = buf[1:].view(B, T)
+train_loader = DataLoaderLite(B=4, T=32)
 
 # model = GPT.from_pretrained('gpt2')
 model = GPT(GPTConfig())
 # print("didn't crash yay")
 
 model.to(device)
-logits, loss = model(x, y)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
-for i in range(5):
+for i in range(10):
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
-    print(f"step {i}, loss: {loss.item():.4f}")
+    print(f"step {i+1}, loss: {loss.item():.4f}")
 
 timer_end()
 
